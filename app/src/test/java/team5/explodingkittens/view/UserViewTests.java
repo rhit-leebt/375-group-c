d6b833d30d53aef9da2b5cd21c6a74140d14a408
Andrew Orians <oriansaj@rose-hulman.edu>
2022-04-22 08:35:43 -0400

    add test for move to front on turn start

--- app/src/test/java/team5/explodingkittens/view/UserViewTests.java
+++ app/src/test/java/team5/explodingkittens/view/UserViewTests.java
@@ -10,20 +10,55 @@ import org.junit.Test;
 
 public class UserViewTests extends ApplicationTest {
 
+    private static class DummyView extends UserView {
+
+        boolean front = false;
+
+        public DummyView(int numPlayers, int playerId, UserViewSceneBuilder builder) {
+            super(numPlayers, playerId, builder);
+        }
+
+        @Override
+        public void toFront() {
+            front = true;
+        }
+    }
     @Override
     public void start(Stage stage) throws Exception {
-//        UserViewSceneBuilder sceneBuilderMock = EasyMock.mock(UserViewSceneBuilder.class);
-//        UserViewSceneHandler handlerMock = EasyMock.mock(UserViewSceneHandler.class);
-//        EasyMock.expect(sceneBuilderMock.generateSceneFromPlayerInfo(2, 0))
-//                .andReturn(handlerMock);
-//        EasyMock.expect(handlerMock.getScene()).andReturn(new Scene(new HBox()));
-//        EasyMock.replay(sceneBuilderMock);
-//        EasyMock.replay(handlerMock);
-//
-//        UserView view = new UserView(2, 0, sceneBuilderMock);
-//        Assert.assertNotNull(view.getScene());
-//
-//        EasyMock.verify(sceneBuilderMock);
+        testConstructor();
+        testMoveToFront();
+    }
+
+    public void testConstructor() {
+        UserViewSceneBuilder sceneBuilderMock = EasyMock.mock(UserViewSceneBuilder.class);
+        UserViewSceneHandler handlerMock = EasyMock.mock(UserViewSceneHandler.class);
+        EasyMock.expect(sceneBuilderMock.generateSceneFromPlayerInfo(2, 0))
+                .andReturn(handlerMock);
+        EasyMock.expect(handlerMock.getScene()).andReturn(new Scene(new HBox()));
+        EasyMock.replay(sceneBuilderMock);
+        EasyMock.replay(handlerMock);
+
+        UserView view = new UserView(2, 0, sceneBuilderMock);
+        Assert.assertNotNull(view.getScene());
+
+        EasyMock.verify(sceneBuilderMock);
+    }
+
+    public void testMoveToFront() {
+        UserViewSceneBuilder sceneBuilderMock = EasyMock.mock(UserViewSceneBuilder.class);
+        UserViewSceneHandler handlerMock = EasyMock.mock(UserViewSceneHandler.class);
+        EasyMock.expect(sceneBuilderMock.generateSceneFromPlayerInfo(2, 0))
+                .andReturn(handlerMock);
+        EasyMock.expect(handlerMock.getScene()).andReturn(new Scene(new HBox()));
+        EasyMock.replay(sceneBuilderMock);
+        EasyMock.replay(handlerMock);
+
+        DummyView view = new DummyView(2, 0, sceneBuilderMock);
+        view.changeUiOnTurnChange(true);
+
+        Assert.assertTrue(view.front);
+        EasyMock.verify(sceneBuilderMock);
+        EasyMock.verify(handlerMock);
     }
 
     @Test
