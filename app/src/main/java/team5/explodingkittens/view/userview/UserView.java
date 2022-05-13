e4fc5cdbd26fcb5ff4a33725b731f5b3f2643159
rhit-leebt <leebt@rose-hulman.edu>
2022-05-13 16:27:17 -0400

    deleted commented out code

--- app/src/main/java/team5/explodingkittens/view/userview/UserView.java
+++ app/src/main/java/team5/explodingkittens/view/userview/UserView.java
@@ -1,9 +1,5 @@
 package team5.explodingkittens.view.userview;
 
-//<<<<<<< HEAD:app/src/main/java/team5/explodingkittens/view/userview/UserView.java
-//=======
-//import java.awt.*;
-//>>>>>>> main:app/src/main/java/team5/explodingkittens/view/UserView.java
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -56,13 +52,7 @@ public class UserView extends Stage implements AbstractUserView {
      * @param playerId   The ID of the player whose window this is.
      */
     public UserView(int numPlayers, int playerId) {
-//<<<<<<< HEAD:app/src/main/java/team5/explodingkittens/view/userview/UserView.java
         UserViewSceneBuilder builder = new UserViewSceneBuilder(generateUserViewEvents());
-//=======
-//        UserViewSceneBuilder builder = new UserViewSceneBuilder(
-//                e -> this.tryPlayCard(),
-//                e -> this.tryDrawCard());
-//>>>>>>> main:app/src/main/java/team5/explodingkittens/view/UserView.java
         sceneHandler = builder.generateSceneFromPlayerInfo(numPlayers, playerId);
         setScene(sceneHandler.getScene());
         show();
--- app/src/main/java/team5/explodingkittens/view/userview/UserViewSceneBuilder.java
+++ app/src/main/java/team5/explodingkittens/view/userview/UserViewSceneBuilder.java
@@ -3,10 +3,6 @@ package team5.explodingkittens.view.userview;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-//<<<<<<< HEAD:app/src/main/java/team5/explodingkittens/view/userview/UserViewSceneBuilder.java
-//=======
-//import javafx.scene.input.MouseEvent;
-//>>>>>>> main:app/src/main/java/team5/explodingkittens/view/UserViewSceneBuilder.java
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import team5.explodingkittens.view.*;
@@ -23,23 +19,12 @@ public class UserViewSceneBuilder {
 
     private UiPlayerHand playerHandUi;
     private List<UiPlayer> playerUis;
-//<<<<<<< HEAD:app/src/main/java/team5/explodingkittens/view/userview/UserViewSceneBuilder.java
     private final UserViewUIParts userViewUIParts;
     private final UserViewEvents userViewEvents;
 
     public UserViewSceneBuilder(UserViewEvents userViewEvents) {
         this.userViewEvents = userViewEvents;
         this.userViewUIParts = new UserViewUIParts();
-//=======
-//    private final EventHandler<ActionEvent> playHandler;
-//    private final EventHandler<MouseEvent> drawHandler;
-//    private final UserViewSceneHandler sceneHandler;
-//
-//    public UserViewSceneBuilder(EventHandler<ActionEvent> playHandler, EventHandler<MouseEvent> drawHandler) {
-//        this.playHandler = playHandler;
-//        this.drawHandler = drawHandler;
-//        this.sceneHandler = new UserViewSceneHandler();
-//>>>>>>> main:app/src/main/java/team5/explodingkittens/view/UserViewSceneBuilder.java
     }
 
     public UserViewSceneHandler generateSceneFromPlayerInfo(int numPlayers, int playerId) {
@@ -53,15 +38,9 @@ public class UserViewSceneBuilder {
 
         HBox alignedUiGroup = getAlignedUiGroup(List.of(otherPlayerUiArea, pileUiArea));
         Scene scene = generateSceneFromUiArea(alignedUiGroup);
-//<<<<<<< HEAD:app/src/main/java/team5/explodingkittens/view/userview/UserViewSceneBuilder.java
-
         UserViewSceneHandler sceneHandler = new UserViewSceneHandler(userViewUIParts, scene);
 
         sceneHandler.setDrawKeyHandler(userViewEvents.drawKeyHandler);
-
-//=======
-//        sceneHandler.replaceScene(scene);
-//>>>>>>> main:app/src/main/java/team5/explodingkittens/view/UserViewSceneBuilder.java
         return sceneHandler;
     }
 
