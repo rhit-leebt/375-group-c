92cfb9538269decc842351859e54cc4f95c5511b
Andrew Orians <oriansaj@rose-hulman.edu>
2022-05-13 14:45:52 -0400

    fxml now works with jar (images broken)

--- app/src/main/java/team5/explodingkittens/view/FXMLLoader.java
+++ app/src/main/java/team5/explodingkittens/view/FXMLLoader.java
@@ -6,14 +6,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class FXMLLoader {
-    private static String FXML_BASE_PATH = "app"
-            + File.separator + "src" + File.separator + "main" + File.separator
-            + "resources" + File.separator + "fxml" + File.separator;
+    private static String FXML_BASE_PATH = "fxml/";
 
     public static void loadFXML(String filename, Object controller) {
         URL fxmlUrl;
         try {
-            fxmlUrl = new File(FXML_BASE_PATH + filename).toURI().toURL();
+            fxmlUrl = FXMLLoader.class.getClassLoader().getResource(FXML_BASE_PATH + filename);
             javafx.fxml.FXMLLoader fxmlLoader = new javafx.fxml.FXMLLoader(fxmlUrl);
             fxmlLoader.setRoot(controller);
             fxmlLoader.setController(controller);
